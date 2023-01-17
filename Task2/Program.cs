c2834064368244989785a430ad1413a3dfbd16c1
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-01-17 15:37:00 +0300

    Task2 homework

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter two numbers ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+//First method
+if(num1 > num2){
+    Console.WriteLine("{0} is max", num1);
+}else if(num2 > num1){
+    Console.WriteLine("{0} is max", num2);
+}else{
+    Console.WriteLine("Numbers are equal");
+}
+//Second method
+Console.WriteLine("Max is {0}",Math.Max(num1,num2));
