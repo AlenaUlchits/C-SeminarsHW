6a3feb018e3d69080e9ff24d76875850c33a8eec
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-01-17 16:25:00 +0300

    Task8 homework

---
+++ Task8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter a number ");
+int num = Convert.ToInt32(Console.ReadLine());
+for(int i = 1;i <=num;i++){
+    if(i % 2==0){
+        Console.Write(i + ", ");
+    }
+}
+Console.Write("\b\b  ");
