54c70ac148164672e5692db9003d7edec912b73d
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-01-20 21:55:30 +0300

    Refactoring

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,31 +1,11 @@
 ﻿void isWeekend(int weekNum){
-    switch(weekNum){
-        case 1:
-        Console.WriteLine("No");
-        break;
-        case 2:
-        Console.WriteLine("No");
-        break;
-        case 3:
-        Console.WriteLine("No");
-        break;
-        case 4:
-        Console.WriteLine("No");
-        break;
-        case 5:
-        Console.WriteLine("No");
-        break;
-        case 6:
-        Console.WriteLine("Yes");
-        break;
-        case 0:
-        Console.WriteLine("Yes");
-        break;
-        default:
-        break;
-    }
+   if(weekNum == 6 || weekNum == 0){
+    Console.WriteLine("It is a weekend");
+   }else{
+    Console.WriteLine("It is not a weekend");
+   }
 }
-Console.Write("Enter a number");
+Console.Write("Enter a number ");
 int num = Convert.ToInt32(Console.ReadLine());
 int numberOfWeek = num % 7;
 isWeekend(numberOfWeek);
