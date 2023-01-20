111b1c47ede1efc5df9a098267f69127decbca32
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-01-20 20:42:49 +0300

    Task 15 with method

---
+++ Task15/Program.cs
@@ -0,0 +1,31 @@
+﻿void showDayOfWeek(int weekNum){
+    switch(weekNum){
+        case 1:
+        Console.WriteLine("Monday");
+        break;
+        case 2:
+        Console.WriteLine("Tuesday");
+        break;
+        case 3:
+        Console.WriteLine("Wednesday");
+        break;
+        case 4:
+        Console.WriteLine("Thursday");
+        break;
+        case 5:
+        Console.WriteLine("Friday");
+        break;
+        case 6:
+        Console.WriteLine("Saturday");
+        break;
+        case 0:
+        Console.WriteLine("Sunday");
+        break;
+        default:
+        break;
+    }
+}
+Console.Write("Enter a number");
+int num = Convert.ToInt32(Console.ReadLine());
+int numberOfWeek = num % 7;
+showDayOfWeek(numberOfWeek);
