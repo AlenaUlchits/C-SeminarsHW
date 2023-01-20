d1f227c497a775b003bd7892455166f0d8bd7d77
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-01-20 20:35:10 +0300

    Task 13 with method

---
+++ Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿void getThirdDigit(string source){
+    source.Substring(0,3);
+    Console.WriteLine(source[2]);
+}
+
+Console.Write("Enter a number ");
+string num = Console.ReadLine();
+if(num != null && num.Length < 3){
+    Console.WriteLine("There is no 3rd digit!");
+}else{
+    getThirdDigit(num);
+}
