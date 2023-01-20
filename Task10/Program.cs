16295b2ae5de9c31b11a2373cc44949f6233c70d
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-01-20 20:20:23 +0300

    Task 10 with function

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿void findSecondNumber(string source){
+char[] arr = source.ToCharArray();
+Console.WriteLine(arr[1]);
+}
+Console.WriteLine("Enter a number ");
+string num = Console.ReadLine();
+if(num.Length == 3){
+    findSecondNumber(num);
+}else{
+    Console.WriteLine("The number doesn't contain 3 charcters");
+}
+
