6811324d032924a0a31a96f6060bbd4e9975a66f
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-01-23 09:51:01 +0300

    new method

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -2,6 +2,12 @@
     source.Substring(0,3);
     Console.WriteLine(source[2]);
 }
+void getThirdDigitNum(int source){
+    while(source>=1000){
+        source /=10;
+    }
+    Console.WriteLine(source%10);
+}
 
 Console.Write("Enter a number ");
 string num = Console.ReadLine();
@@ -9,4 +15,5 @@ if(num != null && num.Length < 3){
     Console.WriteLine("There is no 3rd digit!");
 }else{
     getThirdDigit(num);
+    getThirdDigitNum(Convert.ToInt32(num));
 }
