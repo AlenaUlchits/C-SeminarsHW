bd7391406c4a6eb1d3fd8224a2a8082def935f77
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-01-23 10:32:41 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter a number ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write($"{num} -> ");
+for(int i = 1; i <= num; i++){
+   Console.Write($"{Math.Pow(i, 3)}, "); 
+}
+Console.Write("\b\b. ");
