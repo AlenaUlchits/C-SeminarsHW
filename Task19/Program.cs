fb499aa8428334b39fc6dfb4679692cb4507d224
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-01-23 10:08:43 +0300

    Task 19

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿Boolean checkIsPalindrome(string number){
+    Boolean isPalindrome = false;
+    int lastIndex = number.Length - 1;
+    for(int i = 0; i < number.Length/2; i++){
+        if(number[i] == number[lastIndex-i]){
+            isPalindrome = true;
+        }else{
+            isPalindrome = false;
+        }
+    }
+    return isPalindrome;
+}
+Console.Write("Enter a number ");
+string num = Console.ReadLine();
+Boolean isPalindrome = checkIsPalindrome(num);
+if(isPalindrome){
+    Console.WriteLine("The number {0} is a palindrome.", num);
+}else{
+    Console.WriteLine("The number {0} is not a palindrome.", num);
+}
