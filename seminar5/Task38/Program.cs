f46e56399523139875bbd29c0047adbee8a88209
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-02-03 13:11:03 +0300

    Task 38

---
+++ seminar5/Task38/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] GenerateArray(int size){
+    int[] arr = new int[size];
+    for(int i = 0; i< arr.Length; i++){
+        arr[i] = new Random().Next(-8, 9);
+    }
+    return arr;
+}
+void PrintArray(int[] arr){
+    Console.Write("[ ");
+    for(int i = 0; i< arr.Length; i++){
+
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write("\b\b] ");
+}
+int FindDiffBetwenMaxAndMin(int[] array){
+    int indexMin = 0;
+    int indexMax = 0;
+    for(int i = 1; i < array.Length; i++){
+        if(array[indexMax] < array[i]){
+            indexMax = i;
+        }else if(array[indexMin] > array[i]){
+            indexMin = i;
+        }
+    }
+    int diff = array[indexMax] - array[indexMin];
+    return diff;
+}
+Console.WriteLine("Enter an array size ");
+int size = int.Parse(Console.ReadLine());
+int[] arr = GenerateArray(size);
+PrintArray(arr);
+Console.Write("-> ");
+Console.Write(FindDiffBetwenMaxAndMin(arr));
