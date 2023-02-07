ddfa4e6b0d4f9fa5f3e4a6017a98aa9c53da3bfe
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-02-07 15:48:38 +0300

    Task43

---
+++ seminar6/Task43/Program.cs
@@ -0,0 +1,15 @@
+﻿void FindDot(double[] firstFunctionCoef, double[] secondFunctionCoef){
+    double x = (secondFunctionCoef[0] - firstFunctionCoef[0])/(firstFunctionCoef[1]-secondFunctionCoef[1]);
+    double y = firstFunctionCoef[1] * x + firstFunctionCoef[0];
+    Console.WriteLine($"({Math.Round(x, 2)}, {Math.Round(y, 2)})");
+}
+Console.WriteLine("Enter 2 coefficients of the first function ");
+double[] firstCoef = Console.ReadLine().Split(" ")
+                         .Select(c=>Convert.ToDouble(c))
+                         .ToArray();
+Console.WriteLine("Enter 2 coefficients of the second function ");
+double[] secondCoef = Console.ReadLine().Split(" ")
+                         .Select(c=>Convert.ToDouble(c))
+                         .ToArray();
+Console.Write($"b1 = {firstCoef[0]}, k1 = {firstCoef[1]}, b2 = {secondCoef[0]}, k2 = {secondCoef[1]} ->");
+FindDot(firstCoef, secondCoef);
