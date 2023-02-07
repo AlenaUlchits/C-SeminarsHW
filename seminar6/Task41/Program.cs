136b561fb7ffc07132c7bec7886a44313b4e81a3
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-02-07 14:55:47 +0300

    Task41

---
+++ seminar6/Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿void CountEvenNumbers(int[] array){
+    int count = 0;
+    for(int i = 0; i< array.Length; i++){
+        if(array[i] > 0){
+            count++;
+        }
+    }
+    Console.Write(count);
+}
+void PrintArray(int[] array){
+    for(int i = 0; i < array.Length; i++){
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("\b\b-> ");
+}
+Console.WriteLine("Enter numbers as much as you want ");
+int[] arr = Console.ReadLine().Split(" ")
+                    .Select(n => Convert.ToInt32(n))
+                    .ToArray();
+PrintArray(arr);
+CountEvenNumbers(arr);
