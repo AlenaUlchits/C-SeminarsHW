f5598aabc4c641550ca1f6074c8b4e9ea6a1b5a3
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-02-14 12:43:39 +0300

    Task58

---
+++ seminar8/Task58/Program.cs
@@ -0,0 +1,47 @@
+﻿void PrintArray(int[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i, j], 3}");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] GenerateArray(int n, int m){
+    int[,] arr = new int[n, m];
+    for(int i = 0; i < arr.GetLength(0); i++){
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(0, 5);
+        }
+    }
+    return arr;
+}
+int[,] FindProduct(int[,] matrixA, int[,] matrixB){
+    int[,] result = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+    
+    for(int row = 0; row < matrixA.GetLength(0); row ++){
+        for (int col = 0; col < matrixB.GetLength(1); col++)
+        {
+            for(int colMatA = 0; colMatA < matrixA.GetLength(1); colMatA++){
+                result[row,col] += matrixA[row, colMatA] * matrixB[colMatA, col];
+            }
+        }
+    }
+    return result;
+}
+int n = new Random().Next(3, 6);
+int m = new Random().Next(3, 6);
+int[,] array1 = GenerateArray(n, m);
+n = new Random().Next(3, 6);
+m = new Random().Next(3, 6);
+int[,] array2 = GenerateArray(n, m);
+Console.WriteLine("The first matrix: ");
+PrintArray(array1);
+Console.WriteLine("The second matrix: ");
+PrintArray(array2);
+if(array1.GetLength(1) == array2.GetLength(0)){
+    Console.WriteLine("The product of matrixes");
+    PrintArray(FindProduct(array1, array2));
+}else{
+    Console.WriteLine("Not possible to find product.");
+}
