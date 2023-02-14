712147b78bab8e4332f659068a2a293a5480d0d8
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-02-14 15:01:50 +0300

    Task62

---
+++ seminar8/Task62/Program.cs
@@ -0,0 +1,52 @@
+﻿void PrintArray(int[,] array){
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i, j], 3}");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] GenerateArray(int n, int m){
+    int[,] arr = new int[n, m];
+    int val = 1;
+    int rowInd = 0;
+    int colInd = 0;
+    for(int i = 0; i < n/2; i++){
+
+            while(colInd < m-i){
+                arr[rowInd,colInd] = val;
+                val++;
+                colInd++;              
+            }
+            colInd--;
+            rowInd++;
+            
+            while(rowInd < n-i){
+                arr[rowInd, colInd] = val;
+                val++;
+                rowInd ++;
+            }
+            rowInd--;
+            while(colInd > i){
+                arr[rowInd, colInd-1] = val;
+                val++;
+                colInd--;
+            }
+            while (rowInd>i+1)
+            {
+                arr[rowInd-1, colInd] = val;
+                val++;
+                rowInd--;
+            }
+            colInd++;
+
+    }
+    if(n%2 != 0){
+        int index = n/2;
+        arr[index,index] = val;
+    }
+    return arr;
+}
+int n = new Random().Next(4,5);
+int[,] array = GenerateArray(n, n);
+PrintArray(array);
