ff8abb9e0ebb28eef0efda7494b8a042bc9521cb
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-02-14 15:22:40 +0300

    Task64

---
+++ seminar9/Task64/Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintNum(int n){
+    if(n==1){
+        Console.Write($"{n}");
+        return;
+    }else{
+       Console.Write($"{n}, "); 
+    }  
+    PrintNum(n-1);   
+}
+int n = new Random().Next(4,30);
+Console.Write($"N = {n} -> ");
+PrintNum(n);
+
