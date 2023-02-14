fab9d7376820369e3df96aaa9a94518575832d8d
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-02-14 15:44:04 +0300

    Task66

---
+++ seminar9/Task66/Program.cs
@@ -0,0 +1,13 @@
+﻿int FindSum(int min, int max, int sum){
+    if(min == max){
+        sum += min;
+        return sum;
+    }else{
+        sum +=min;
+       return FindSum(min+1, max, sum);
+    }
+}
+int min = new Random().Next(1,15);
+int max = new Random().Next(min+1,30);
+int sum = 0;
+Console.WriteLine($"Sum between {min} and {max} equals {FindSum(min,max, sum)}");
