c2fab7285d36b72139c64e12f2ac675614af02f9
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-02-15 14:38:58 +0300

    Task68

---
+++ seminar9/Task68/Program.cs
@@ -0,0 +1,19 @@
+﻿int Ackermann(int m, int n){
+    if(m==0){
+        return n +1;
+    }else if(m > 0 && n==0){
+        return Ackermann(m-1,1);
+    }else{
+        return Ackermann(m-1, Ackermann(m, n-1));
+    }
+
+}
+int m = new Random().Next(0,4);
+int n = new Random().Next(0,11);
+if(m < 0 || n < 0){
+    Console.WriteLine($"Not possible to find A({m},{n})");
+}else{
+    int result = Ackermann(m,n); 
+  Console.WriteLine($"A({m},{n}) -> {result}");  
+}
+
